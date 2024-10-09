c6c6b6b69cd76cf1ad545c81469a76c38ea0a658
Javkhlant Altankhuyag <enrosek95@gmail.com>
2024-10-09 21:54:33 +0800

    Add theme changer button

--- lib/components/theme_changer.dart
+++ lib/components/theme_changer.dart
@@ -1,10 +1,79 @@
+import 'package:calculator_app/providers/theme_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:provider/provider.dart';
 
 class ThemeChanger extends StatelessWidget {
   const ThemeChanger({super.key});
 
+  void handleTap(BuildContext context) {
+    context.read<ThemeProvider>().toggleTheme();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Text("Theme Changer");
+    final bool isLightTheme =
+        context.watch<ThemeProvider>().themeMode == ThemeMode.light;
+
+    return GestureDetector(
+      onTap: () {
+        handleTap(context);
+      },
+      child: Container(
+        width: 72,
+        height: 32,
+        padding: const EdgeInsets.all(4),
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.tertiary,
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisSize: MainAxisSize.max,
+          children: isLightTheme
+              ? [
+                  SvgPicture.asset(
+                    "assets/icons/sun.svg",
+                    width: 24,
+                    height: 24,
+                    colorFilter: ColorFilter.mode(
+                      Theme.of(context).colorScheme.primary.withOpacity(0.7),
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondary,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  )
+                ]
+              : [
+                  Container(
+                    width: 24,
+                    height: 24,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).colorScheme.secondary,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  ),
+                  const SizedBox(width: 12),
+                  SvgPicture.asset(
+                    "assets/icons/moon.svg",
+                    width: 24,
+                    height: 24,
+                    colorFilter: ColorFilter.mode(
+                      Theme.of(context).colorScheme.primary.withOpacity(0.7),
+                      BlendMode.srcIn,
+                    ),
+                  ),
+                ],
+        ),
+      ),
+    );
   }
 }
